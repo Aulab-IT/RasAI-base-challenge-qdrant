ba76f01acba6c2dfb8c3b252a11fd2935b2621ed
Marco Insabato <marcoins95@gmail.com>
2024-05-13 16:27:29 +0200

    aggiunta favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:wght@400;500;600;700;900&display=swap" rel="stylesheet">
 
+    {{-- Faavicon --}}
+    <link rel="icon" href="/RagsAI-LOGO.png" type="image/x-icon" />
+
     @vite(['resources/css/app.css' , 'resources/js/app.js'])
   </head>
   <body data-bs-theme="dark">
